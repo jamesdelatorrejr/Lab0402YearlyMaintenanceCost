2b979472174d53f6d64ac3d9122ddd417e506133
JamesyLive <jamesjrbus@gmail.com>
2023-05-15 16:18:50 -0400

    Put code for task 2.

--- src/Lab0402YearlyMaintenanceCost.java
+++ src/Lab0402YearlyMaintenanceCost.java
@@ -1,5 +1,16 @@
-public class Lab0402YearlyMaintenanceCost {
-    public static void main(String[] args) {
+public class Lab0402YearlyMaintenanceCost
+{
+    public static void main(String[] args)
+    {
+        double springCost = 125.73;
+        double summerCost = 759.24;
+        double fallCost = 234.22;
+        double winterCost = 252.22;
+        double yearCost = 0;
 
+        yearCost = springCost + summerCost + fallCost + winterCost;
+
+        System.out.println("Your cost for the Spring is " + springCost + ". " + "Your cost for the Summer is " + summerCost + ". " + "Your cost for the Fall is " + fallCost + ". "
+          + "Your cost for the Winter is " + winterCost + ". " + "Your total cost is " + yearCost + ".");
     }
 }
